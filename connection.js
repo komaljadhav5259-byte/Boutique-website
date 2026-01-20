9018b84f593b79c7c215508d77635652dfcf87e2
JADHAV KOMAL BAPU <komaljadhav5259@gmail.com>
2026-01-20 11:10:48 +0530

    Update MySQL connection details

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "boutique"
+    host: "blwkncl5qtrxudus3obt-mysql.services.clever-cloud.com",
+    user: "ubz56qok9icxw5mf",
+    password: "jnDNiubNCtU7ZxpIgRgP",
+    database: "blwkncl5qtrxudus3obt"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
